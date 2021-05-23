16d1120783f8cc8e39217bf5af2caf811ab70d22
Parkar99 <nabeelparkar99@gmail.com>
2021-05-23 23:43:39 +0530

    change: Dependencies to dev

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,14 +1,11 @@
 import 'dart:async';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:fnotes/injection.dart';
 import 'package:fnotes/persentation/core/main.dart';
 import 'package:injectable/injectable.dart';
 
 Future<void> main() async {
-  final String environment = kDebugMode ? Environment.dev : Environment.prod;
-  configureDependencies(environment);
-
+  configureDependencies(Environment.dev);
   runApp(const Main());
 }
