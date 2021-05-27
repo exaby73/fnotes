bde0766e086cf4730f2a2fa302e0383bdf237136
Parkar99 <nabeelparkar99@gmail.com>
2021-05-27 23:03:44 +0530

    add: Google fonts

--- client/lib/presentation/core/main.dart
+++ client/lib/presentation/core/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:fnotes/presentation/core/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Main extends HookWidget {
   const Main();
 
   @override
   Widget build(BuildContext context) {
+    final darkThemeData = useMemoized(() => ThemeData.dark());
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'FNotes',
       initialRoute: '/login',
+      theme: darkThemeData.copyWith(
+        primaryColor: Colors.orange,
+        primaryColorDark: Colors.orange,
+        accentColor: Colors.orangeAccent,
+        textTheme: GoogleFonts.quicksandTextTheme(darkThemeData.textTheme),
+      ),
       onGenerateRoute: router.generator,
     );
   }
