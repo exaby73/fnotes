c089608e1760e3bae8cab1d1f073e582842d9d90
Parkar99 <nabeelparkar99@gmail.com>
2021-06-20 16:30:51 +0530

    WIP

--- client/lib/presentation/core/routes.dart
+++ client/lib/presentation/core/routes.dart
@@ -4,6 +4,7 @@ import 'package:flutter/scheduler.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:fnotes/application/auth/sign_in_form_cubit.dart';
 import 'package:fnotes/injection.dart';
+import 'package:fnotes/log.dart';
 import 'package:fnotes/presentation/auth/pages/login/sign_in_page.dart';
 import 'package:fnotes/presentation/notes/pages/notes_list.dart';
 
@@ -12,19 +13,26 @@ FluroRouter router = FluroRouter();
 class RoutePaths {
   const RoutePaths._();
 
+  static const notFound = '/not_found';
   static const notesList = '/';
   static const signIn = '/sign-in';
   static const signUp = '/sign-up';
 }
 
 void defineRoutes() {
+  router.define(RoutePaths.notFound, handler: Handler(
+    handlerFunc: (context, parameters) {
+      return const NotFoundPage();
+    },
+  ), transitionType: TransitionType.inFromRight);
+
   router.define(RoutePaths.signIn, handler: Handler(
     handlerFunc: (context, parameters) {
       return SignInFormCubit.to.state.formState.maybeWhen(
         success: () {
           if (context == null) return const SignInPage();
           SchedulerBinding.instance?.addPostFrameCallback((_) {
-            router.navigateTo(context, RoutePaths.notesList);
+            router.navigateTo(context, RoutePaths.notesList, clearStack: true);
           });
 
           return const Scaffold();
@@ -39,23 +47,40 @@ void defineRoutes() {
   router.define(RoutePaths.notesList, handler: Handler(
     handlerFunc: (context, parameters) {
       if (context == null) return const NotFoundPage();
-      return authenticatedNavigateTo(context, const NotesList());
+      return authenticatedNavigateTo(
+        context,
+        const NotesList(),
+        clearStack: true,
+      );
     },
   ), transitionType: TransitionType.inFromRight);
 
   router.notFoundHandler = Handler(
+    type: HandlerType.function,
     handlerFunc: (context, parameters) {
-      return const NotFoundPage();
+      if (context == null) return;
+      router.navigateTo(context, RoutePaths.notFound);
     },
   );
 }
 
-Widget authenticatedNavigateTo(BuildContext context, Widget page) {
+Widget authenticatedNavigateTo(
+  BuildContext context,
+  Widget page, {
+  bool clearStack = false,
+}) {
+  logd('Here');
   return getIt<SignInFormCubit>().state.formState.maybeWhen(
-        success: () => page,
+        success: () {
+          return page;
+        },
         orElse: () {
           SchedulerBinding.instance?.addPostFrameCallback((_) {
-            router.navigateTo(context, RoutePaths.signIn);
+            router.navigateTo(
+              context,
+              RoutePaths.signIn,
+              clearStack: clearStack,
+            );
           });
 
           return const Scaffold();
